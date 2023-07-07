0fe4fd36ea678779dfd7d26aad695fcd33857afd
abhishek saini <abhisheksaini388@gmail.com>
2023-07-07 17:21:01 +0530

    data_ingestion&yamlfile

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from  cnnClassifier import logger
+from  src.cnnClassifier import logger
 import json
 from ensure import ensure_annotations
 from box import ConfigBox
