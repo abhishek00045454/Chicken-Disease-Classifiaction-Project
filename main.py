8851373959afde1db2f2da6a05edf72554bdba16
abhishek saini <abhisheksaini388@gmail.com>
2023-07-10 19:01:18 +0530

    training_model&call_back_model.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_preapre_base_model import PrepareBaseModelTrainingPipeline
-
-
+from src.cnnClassifier.pipeline.stage_03_tarinng_pipeline import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -28,4 +27,15 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
